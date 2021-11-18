0c0f21ec20ed1a8cd37b4c3cafb28eb9a0e545cd
Nina Bildirici <nrb722@gmail.com>
2021-11-18 15:15:55 -0500

    Filled in methods in OurHashMap Class

--- src/main/java/OurHashMap.java
+++ src/main/java/OurHashMap.java
@@ -1,2 +1,167 @@
-package PACKAGE_NAME;public class OurHashMap {
+import java.util.*;
+
+public class OurHashMap<K, V> implements Map<K, V> {
+
+    private final int SIZE = 16;
+
+    class Entry<K, V> {
+        K key;
+        V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    List<Entry> values[] = new List[SIZE];
+
+    @Override
+    public int size() {
+        int size = 0;
+        for (List<Entry> value : values) {
+            if (value != null) {
+                for (Entry entry : value) {
+                    size++;
+                }
+            }
+        }
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        for (List<Entry> value : values) {
+            if (value != null) {
+                for (Entry entry : value) {
+                    if (entry.key == key) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        for (List<Entry> val : values) {
+            if (val != null) {
+                for (Entry entry : val) {
+                    if (entry.value == value) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        int hashcode = key.hashCode();
+        int index = Math.abs(hashcode) % SIZE;
+        List<Entry> list = values[index];
+        if (list == null) {
+            return null;
+        }
+        for (Entry entry : list) {
+            if (entry.key.equals(key)) {
+                return (V) entry.value;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        int hashcode = key.hashCode();
+        int index = Math.abs(hashcode) % SIZE;
+        List list = values[index];
+        if (list == null) {
+            list = new ArrayList<Entry>();
+            values[index] = list;
+        }
+
+        for (Entry<K, V> entry : (List<Entry<K, V>>) list) {
+            if (entry.key.equals(key)) {
+                V saved = entry.value;
+                entry.value = value;
+                return saved;
+            }
+        }
+
+        Entry entry = new Entry(key, value);
+        list.add(entry);
+
+        return null;
+    }
+
+    @Override
+    public V remove(Object key) {
+        V saved = null;
+        for (List<Entry> val : values) {
+            if (val != null) {
+                for (Entry entry : val) {
+                    if (entry.key == key) {
+                        saved = (V) entry.value;
+                        val.remove(entry);
+                    }
+                }
+            }
+        }
+        return saved;
+    }
+
+    @Override
+    public void putAll(Map m) {
+        for (List<Entry> val : values) {
+            if (val != null) {
+                for (Entry entry : val) {
+                    m.put(entry.key, entry.value);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void clear() {
+        values = new List[SIZE];
+    }
+
+    @Override
+    public Set keySet() {
+        Set set = new HashSet();
+        for (List<Entry> val : values) {
+            if (val != null) {
+                for (Entry entry : val) {
+                    set.add(entry.key);
+                }
+            }
+        }
+        return set;
+    }
+
+    @Override
+    public Collection values() {
+        Collection collection = new ArrayList();
+        for (List<Entry> val : values) {
+            if (val != null) {
+                for (Entry entry : val) {
+                    collection.add(entry.value);
+                }
+            }
+        }
+        return collection;
+    }
+
+    @Override
+    public Set<java.util.Map.Entry<K, V>> entrySet() {
+        return null;
+    }
 }
--- src/test/java/OurHashMapTest.java
+++ src/test/java/OurHashMapTest.java
@@ -1,4 +1,278 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class OurHashMapTest {
-  
+
+    @Test
+    void getNull() {
+        // given
+        OurHashMap<String, String> map = new OurHashMap<>();
+
+        // when
+
+        // then
+        assertNull(map.get("ENGLISH1"));
+    }
+
+    @Test
+    void put() {
+        // given
+        OurHashMap<String, String> map = new OurHashMap<>();
+
+        // when
+        map.put("ENGLISH1", "HELLO");
+        map.put("ENGLISH2", "HI");
+        map.put("ENGLISH3", "HEY");
+        map.put("SPANISH", "HOLA");
+        map.put("HEBREW", "SHALOM");
+        map.put("FRENCH", "BONJOUR");
+        map.put("RUSSIAN", "PRIVIT");
+        map.put("JAPANESE", "NE HOW");
+        map.put("MANDARIN", "CONICHIWA");
+
+        // then
+        assertEquals("HELLO", map.get("ENGLISH1"));
+        assertEquals("HI", map.get("ENGLISH2"));
+        assertEquals("HEY", map.get("ENGLISH3"));
+        assertEquals("HOLA", map.get("SPANISH"));
+        assertEquals("SHALOM", map.get("HEBREW"));
+        assertEquals("BONJOUR", map.get("FRENCH"));
+        assertEquals("PRIVIT", map.get("RUSSIAN"));
+        assertEquals("NE HOW", map.get("JAPANESE"));
+        assertEquals("CONICHIWA", map.get("MANDARIN"));
+    }
+
+    @Test
+    void putSameKeys() {
+        // given
+        OurHashMap<String, String> map = new OurHashMap<>();
+
+        // when
+        map.put("ENGLISH1", "HELLO");
+        map.put("SPANISH", "HOLA");
+        map.put("HEBREW", "SHALOM");
+        map.put("FRENCH", "BONJOUR");
+        map.put("ENGLISH1", "HI");
+
+        // then
+        assertEquals("HI", map.get("ENGLISH1"));
+    }
+
+    @Test
+    void size() {
+        // given
+        OurHashMap<String, String> map = new OurHashMap<>();
+
+        // when
+        map.put("ENGLISH1", "HELLO");
+        map.put("SPANISH", "HOLA");
+        map.put("HEBREW", "SHALOM");
+        map.put("FRENCH", "BONJOUR");
+
+
+        // then
+        assertEquals(4, map.size());
+
+    }
+
+    @Test
+    void isEmptyFalse() {
+        // given
+        OurHashMap<String, String> map = new OurHashMap<>();
+
+        // when
+        map.put("ENGLISH1", "HELLO");
+        map.put("SPANISH", "HOLA");
+        map.put("HEBREW", "SHALOM");
+        map.put("FRENCH", "BONJOUR");
+
+
+        // then
+        assertFalse(map.isEmpty());
+
+    }
+
+    @Test
+    void isEmpty() {
+        // given
+        OurHashMap<String, String> map = new OurHashMap<>();
+
+        // then
+        assertTrue(map.isEmpty());
+
+    }
+
+    @Test
+    void containsKey() {
+        // given
+        OurHashMap<String, String> map = new OurHashMap<>();
+
+        // when
+        map.put("ENGLISH1", "HELLO");
+        map.put("SPANISH", "HOLA");
+        map.put("HEBREW", "SHALOM");
+        map.put("FRENCH", "BONJOUR");
+
+
+        // then
+        assertTrue(map.containsKey("ENGLISH1"));
+    }
+
+    @Test
+    void containsKeyFalse() {
+        // given
+        OurHashMap<String, String> map = new OurHashMap<>();
+
+        // when
+        map.put("ENGLISH1", "HELLO");
+        map.put("SPANISH", "HOLA");
+        map.put("HEBREW", "SHALOM");
+        map.put("FRENCH", "BONJOUR");
+
+
+        // then
+        assertFalse(map.containsKey("ENGLISH123"));
+    }
+
+    @Test
+    void containsValue() {
+        // given
+        OurHashMap<String, String> map = new OurHashMap<>();
+
+        // when
+        map.put("ENGLISH1", "HELLO");
+        map.put("SPANISH", "HOLA");
+        map.put("HEBREW", "SHALOM");
+        map.put("FRENCH", "BONJOUR");
+
+
+        // then
+        assertTrue(map.containsValue("HELLO"));
+    }
+
+    @Test
+    void containsValueFalse() {
+        // given
+        OurHashMap<String, String> map = new OurHashMap<>();
+
+        // when
+        map.put("ENGLISH1", "HELLO");
+        map.put("SPANISH", "HOLA");
+        map.put("HEBREW", "SHALOM");
+        map.put("FRENCH", "BONJOUR");
+
+
+        // then
+        assertFalse(map.containsValue("HELLO123"));
+    }
+
+
+    @Test
+    void remove() {
+        // given
+        OurHashMap<String, String> map = new OurHashMap<>();
+
+        // when
+        map.put("ENGLISH1", "HELLO");
+        map.put("SPANISH", "HOLA");
+        map.put("HEBREW", "SHALOM");
+        map.put("FRENCH", "BONJOUR");
+
+
+        // then
+        assertEquals("HELLO", map.remove("ENGLISH1"));
+        assertFalse(map.containsKey("ENGLISH1"));
+        assertFalse(map.containsValue("HELLO"));
+        assertEquals(3, map.size());
+    }
+
+
+    @Test
+    void putAll() {
+        // given
+        OurHashMap<String, String> map = new OurHashMap<>();
+        OurHashMap<String, String> m = new OurHashMap<>();
+
+        // when
+        map.put("ENGLISH1", "HELLO");
+        map.put("SPANISH", "HOLA");
+        map.put("HEBREW", "SHALOM");
+        map.put("FRENCH", "BONJOUR");
+
+        map.putAll(m);
+
+
+        // then
+        assertEquals(4, m.size());
+        assertTrue(m.containsKey("ENGLISH1"));
+    }
+
+    @Test
+    void clear() {
+        // given
+        OurHashMap<String, String> map = new OurHashMap<>();
+
+        // when
+        map.put("ENGLISH1", "HELLO");
+        map.put("SPANISH", "HOLA");
+        map.put("HEBREW", "SHALOM");
+        map.put("FRENCH", "BONJOUR");
+
+        map.clear();
+
+        // then
+        assertEquals(0, map.size());
+        assertFalse(map.containsValue("HELLO"));
+    }
+
+
+    @Test
+    void keySet() {
+        // given
+        OurHashMap<String, String> map = new OurHashMap<>();
+        Set set = new HashSet();
+
+        // when
+        map.put("ENGLISH1", "HELLO");
+        map.put("SPANISH", "HOLA");
+        map.put("HEBREW", "SHALOM");
+        map.put("FRENCH", "BONJOUR");
+
+        set = map.keySet();
+
+        // then
+        assertEquals(4, set.size());
+        assertTrue(set.contains("ENGLISH1"));
+        assertFalse(set.contains("HELLO"));
+    }
+
+    @Test
+    void values() {
+        // given
+        OurHashMap<String, String> map = new OurHashMap<>();
+        Collection collection = new ArrayList();
+
+        // when
+        map.put("ENGLISH1", "HELLO");
+        map.put("SPANISH", "HOLA");
+        map.put("HEBREW", "SHALOM");
+        map.put("FRENCH", "BONJOUR");
+
+        collection = map.values();
+
+        // then
+        assertEquals(4, collection.size());
+        assertTrue(collection.contains("HELLO"));
+        assertFalse(collection.contains("ENGLISH1"));
+    }
+
 }
+
+
+
